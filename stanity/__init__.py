e61556007f7737784b0dc239c1e1c33eda1660bc
Jacki Buros <jackinovik@gmail.com>
2016-06-06 15:34:24 +0000

    Revert "fix indents"
    This reverts commit ec66731aa3857359d8d8e4b39118f787c47f983c.

--- stanity/__init__.py
+++ stanity/__init__.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import
 
 from .fit import fit
-from .psisloo import Psisloo, psisloo, loo_compare
+from .psisloo import Psisloo, psisloo
 
 __all__ = [
     "fit",
     "psisloo",
     "Psisloo",
-    "loo_compare",
 ]
