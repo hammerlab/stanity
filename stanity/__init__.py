4f38d084cb08ea10b4cd807d2340c6e5cbaf8a8e
Jacki Buros <jackinovik@gmail.com>
2016-06-06 16:44:58 +0000

    added prict_dict helper to package

--- stanity/__init__.py
+++ stanity/__init__.py
@@ -2,10 +2,12 @@ from __future__ import absolute_import
 
 from .fit import fit
 from .psisloo import Psisloo, psisloo, loo_compare
+from .utilities import print_dict
 
 __all__ = [
     "fit",
     "psisloo",
     "Psisloo",
     "loo_compare",
+    "print_dict",
 ]
