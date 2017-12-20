01cd59ff342c18451fb6a0b0864f0b12206f2453
Jacqueline Buros <jburos@penguinrandomhouse.com>
2017-12-20 15:01:39 -0500

    update psis.py to latest version from avehtari/PSIS@cac66c0

--- stanity/psis.py
+++ stanity/psis.py
@@ -1,7 +1,7 @@
 """Pareto smoothed importance sampling (PSIS)
 
 This module implements Pareto smoothed importance sampling (PSIS) and PSIS
-leave-one-out cross-validation for Python (Numpy).
+leave-one-out (LOO) cross-validation for Python (Numpy).
 
 Included functions
 ------------------
@@ -22,58 +22,79 @@ sumlogs
 
 References
 ----------
-Aki Vehtari, Andrew Gelman and Jonah Gabry (2015). Efficient implementation
-of leave-one-out cross-validation and WAIC for evaluating fitted Bayesian
-models. arXiv preprint arXiv:1507.04544.
+Aki Vehtari, Andrew Gelman and Jonah Gabry (2017). Practical
+Bayesian model evaluation using leave-one-out cross-validation
+and WAIC. Statistics and Computing, 27(5):1413–1432.
+doi:10.1007/s11222-016-9696-4. https://arxiv.org/abs/1507.04544
 
-Aki Vehtari and Andrew Gelman (2015). Pareto smoothed importance sampling.
-arXiv preprint arXiv:1507.02646.
+Aki Vehtari, Andrew Gelman and Jonah Gabry (2017). Pareto
+smoothed importance sampling. https://arxiv.org/abs/arXiv:1507.02646v5
 
 """
-
-# Copyright (c) 2015 Aki Vehtari, Tuomas Sivula
-# Original Matlab version by Aki Vehtari. Translation to Python
-# by Tuomas Sivula.
-
-# This software is distributed under the GNU General Public
-# License (version 3 or later); please refer to the file
-# License.txt, included with the software, for details.
 
 from __future__ import division # For Python 2 compatibility
 import numpy as np
 
+# 3-Clause BSD License
+"""
+Copyright 2017 Aki Vehtari, Tuomas Sivula
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. """
+
 
 def psisloo(log_lik, **kwargs):
-    """PSIS leave-one-out log predictive densities.
-    
-    Computes the log predictive densities given posterior samples of the log 
-    likelihood terms p(y_i|\theta^s) in input parameter `log_lik`. Returns a 
-    sum of the leave-one-out log predictive densities `loo`, individual 
-    leave-one-out log predictive density terms `loos` and an estimate of Pareto 
-    tail indeces `ks`. If tail index k>0.5, variance of the raw estimate does 
-    not exist and if tail index k>1 the mean of the raw estimate does not exist 
-    and the PSIS estimate is likely to have large variation and some bias.
-    
+    r"""PSIS leave-one-out log predictive densities.
+
+    Computes the log predictive densities given posterior samples of the log
+    likelihood terms :math:`p(y_i|\theta^s)` in input parameter `log_lik`.
+    Returns a sum of the leave-one-out log predictive densities `loo`,
+    individual leave-one-out log predictive density terms `loos` and an estimate
+    of Pareto tail indeces `ks`. The estimates are unreliable if tail index
+    ``k > 0.7`` (see more in the references listed in the module docstring).
+
+    Additional keyword arguments are passed to the :meth:`psislw()` function
+    (see the corresponding documentation).
+
     Parameters
     ----------
     log_lik : ndarray
         Array of size n x m containing n posterior samples of the log likelihood
-        terms p(y_i|\theta^s).
-    
-    Additional keyword arguments are passed to the psislw() function (see the
-    corresponding documentation).
-    
+        terms :math:`p(y_i|\theta^s)`.
+
     Returns
     -------
     loo : scalar
         sum of the leave-one-out log predictive densities
-    
+
     loos : ndarray
         individual leave-one-out log predictive density terms
-    
+
     ks : ndarray
         estimated Pareto tail indeces
-    
+
     """
     # ensure overwrite flag in passed arguments
     kwargs['overwrite_lw'] = True
@@ -88,33 +109,29 @@ def psisloo(log_lik, **kwargs):
     return loo, loos, ks
 
 
-def psislw(lw, wcpp=20, wtrunc=3/4, overwrite_lw=False):
+def psislw(lw, Reff=1.0, overwrite_lw=False):
     """Pareto smoothed importance sampling (PSIS).
-    
+
     Parameters
     ----------
     lw : ndarray
         Array of size n x m containing m sets of n log weights. It is also
         possible to provide one dimensional array of length n.
-    
-    wcpp : number
-        Percentage of samples used for GPD fit estimate (default is 20).
-    
-    wtrunc : float
-        Positive parameter for truncating very large weights to n^wtrunc.
-        Providing False or 0 disables truncation. Default values is 3/4.
-    
+
+    Reff : scalar, optional
+        relative MCMC efficiency ``N_eff / N``
+
     overwrite_lw : bool, optional
-        If True, the input array `lw` is smoothed in-place. By default, a new
-        array is allocated.
-    
+        If True, the input array `lw` is smoothed in-place, assuming the array
+        is F-contiguous. By default, a new array is allocated.
+
     Returns
     -------
     lw_out : ndarray
         smoothed log weights
     kss : ndarray
         Pareto tail indices
-    
+
     """
     if lw.ndim == 2:
         n, m = lw.shape
@@ -125,28 +142,32 @@ def psislw(lw, wcpp=20, wtrunc=3/4, overwrite_lw=False):
         raise ValueError("Argument `lw` must be 1 or 2 dimensional.")
     if n <= 1:
         raise ValueError("More than one log-weight needed.")
-    
-    if overwrite_lw:
+
+    if overwrite_lw and lw.flags.f_contiguous:
         # in-place operation
         lw_out = lw
     else:
         # allocate new array for output
-        lw_out = np.copy(lw, order='K')
-    
+        lw_out = np.copy(lw, order='F')
+
     # allocate output array for kss
     kss = np.empty(m)
-    
+
     # precalculate constants
+    cutoff_ind = - int(np.ceil(min(0.2 * n, 3 * np.sqrt(n / Reff)))) - 1
     cutoffmin = np.log(np.finfo(float).tiny)
     logn = np.log(n)
-    
+    k_min = 1/3
+
     # loop over sets of log weights
-    for i, x in enumerate(lw_out.T if lw_out.ndim == 2 else lw_out[None,:]):
+    for i, x in enumerate(lw_out.T if lw_out.ndim == 2 else lw_out[None, :]):
         # improve numerical accuracy
         x -= np.max(x)
+        # sort the array
+        x_sort_ind = np.argsort(x)
         # divide log weights into body and right tail
         xcutoff = max(
-            np.percentile(x, 100 - wcpp),
+            x[x_sort_ind[cutoff_ind]],
             cutoffmin
         )
         expxcutoff = np.exp(xcutoff)
@@ -163,6 +184,8 @@ def psislw(lw, wcpp=20, wtrunc=3/4, overwrite_lw=False):
             np.exp(x2, out=x2)
             x2 -= expxcutoff
             k, sigma = gpdfitnew(x2, sort=x2si)
+        if k >= k_min and not np.isinf(k):
+            # no smoothing if short tail or GPD fit failed
             # compute ordered statistic for the fit
             sti = np.arange(0.5, n2)
             sti /= n2
@@ -171,98 +194,108 @@ def psislw(lw, wcpp=20, wtrunc=3/4, overwrite_lw=False):
             np.log(qq, out=qq)
             # place the smoothed tail into the output array
             x[tailinds[x2si]] = qq
-        if wtrunc > 0:
-            # truncate too large weights
-            lwtrunc = wtrunc * logn - logn + sumlogs(x)
-            x[x > lwtrunc] = lwtrunc
+            # truncate smoothed values to the largest raw weight 0
+            x[x > 0] = 0
         # renormalize weights
         x -= sumlogs(x)
         # store tail index k
         kss[i] = k
-    
+
     # If the provided input array is one dimensional, return kss as scalar.
     if lw_out.ndim == 1:
         kss = kss[0]
-    
+
     return lw_out, kss
 
 
-def gpdfitnew(x, sort=True):
+def gpdfitnew(x, sort=True, sort_in_place=False, return_quadrature=False):
     """Estimate the paramaters for the Generalized Pareto Distribution (GPD)
-    
+
     Returns empirical Bayes estimate for the parameters of the two-parameter
     generalized Parato distribution given the data.
-    
+
     Parameters
     ----------
     x : ndarray
         One dimensional data array
-    
-    sort : {bool, ndarray, 'in-place'}, optional
+
+    sort : bool or ndarray, optional
         If known in advance, one can provide an array of indices that would
         sort the input array `x`. If the input array is already sorted, provide
-        False. If the array is not sorted but can be sorted in-place, provide
-        string 'in-place'. If True (default behaviour) the sorted array indices
-        are determined internally.
-    
+        False. If True (default behaviour), the array is sorted internally.
+
+    sort_in_place : bool, optional
+        If `sort` is True and `sort_in_place` is True, the array is sorted
+        in-place (False by default).
+
+    return_quadrature : bool, optional
+        If True, quadrature points and weight `ks` and `w` of the marginal posterior distribution of k are also calculated and returned. False by
+        default.
+
     Returns
     -------
     k, sigma : float
         estimated parameter values
-    
+
+    ks, w : ndarray
+        Quadrature points and weights of the marginal posterior distribution
+        of `k`. Returned only if `return_quadrature` is True.
+
     Notes
     -----
     This function returns a negative of Zhang and Stephens's k, because it is
     more common parameterisation.
-    
+
     """
     if x.ndim != 1 or len(x) <= 1:
         raise ValueError("Invalid input array.")
-    
+
     # check if x should be sorted
     if sort is True:
-        sort = np.argsort(x)
-        xsorted = False
+        if sort_in_place:
+            x.sort()
+            xsorted = True
+        else:
+            sort = np.argsort(x)
+            xsorted = False
     elif sort is False:
-        xsorted = True
-    elif sort == 'in-place':
-        x.sort()
         xsorted = True
     else:
         xsorted = False
-    
+
     n = len(x)
-    m = 80 + int(np.floor(np.sqrt(n)))
-    
+    PRIOR = 3
+    m = 30 + int(np.sqrt(n))
+
     bs = np.arange(1, m + 1, dtype=float)
     bs -= 0.5
     np.divide(m, bs, out=bs)
     np.sqrt(bs, out=bs)
     np.subtract(1, bs, out=bs)
     if xsorted:
-        bs /= 3 * x[np.floor(n/4 + 0.5) - 1]
+        bs /= PRIOR * x[int(n/4 + 0.5) - 1]
         bs += 1 / x[-1]
     else:
-        bs /= 3 * x[sort[np.floor(n/4 + 0.5) - 1]]
+        bs /= PRIOR * x[sort[int(n/4 + 0.5) - 1]]
         bs += 1 / x[sort[-1]]
-    
+
     ks = np.negative(bs)
     temp = ks[:,None] * x
     np.log1p(temp, out=temp)
     np.mean(temp, axis=1, out=ks)
-    
+
     L = bs / ks
     np.negative(L, out=L)
     np.log(L, out=L)
     L -= ks
     L -= 1
     L *= n
-    
+
     temp = L - L[:,None]
     np.exp(temp, out=temp)
     w = np.sum(temp, axis=1)
     np.divide(1, w, out=w)
-    
+
     # remove negligible weights
     dii = w >= 10 * np.finfo(float).eps
     if not np.all(dii):
@@ -270,7 +303,7 @@ def gpdfitnew(x, sort=True):
         bs = bs[dii]
     # normalise w
     w /= w.sum()
-    
+
     # posterior mean for b
     b = np.sum(bs * w)
     # Estimate for k, note that we return a negative of Zhang and
@@ -278,10 +311,24 @@ def gpdfitnew(x, sort=True):
     temp = (-b) * x
     np.log1p(temp, out=temp)
     k = np.mean(temp)
+    if return_quadrature:
+        np.negative(x, out=temp)
+        temp = bs[:, None] * temp
+        np.log1p(temp, out=temp)
+        ks = np.mean(temp, axis=1)
     # estimate for sigma
-    sigma = -k / b
-    
-    return k, sigma
+    sigma = -k / b * n / (n - 0)
+    # weakly informative prior for k
+    a = 10
+    k = k * n / (n+a) + a * 0.5 / (n+a)
+    if return_quadrature:
+        ks *= n / (n+a)
+        ks += a * 0.5 / (n+a)
+
+    if return_quadrature:
+        return k, sigma, ks, w
+    else:
+        return k, sigma
 
 
 def gpinv(p, k, sigma):
@@ -331,10 +378,10 @@ def gpinv(p, k, sigma):
 
 def sumlogs(x, axis=None, out=None):
     """Sum of vector where numbers are represented by their logarithms.
-    
-    Calculates np.log(np.sum(np.exp(x), axis=axis)) in such a fashion that it
-    works even when elements have large magnitude.
-    
+
+    Calculates ``np.log(np.sum(np.exp(x), axis=axis))`` in such a fashion that
+    it works even when elements have large magnitude.
+
     """
     maxx = x.max(axis=axis, keepdims=True)
     xnorm = x - maxx
@@ -346,4 +393,3 @@ def sumlogs(x, axis=None, out=None):
         out = np.log(out)
     out += np.squeeze(maxx)
     return out
-
